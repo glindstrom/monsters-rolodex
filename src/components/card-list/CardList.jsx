06f9e11d5c91e88f6c75342d85c6077f1fbd9f04
Gabriel Lindström <gabriel.lindstrom@gmail.com>
2020-07-17 17:13:05 +0300

    Add files for github pages

---
+++ src/components/card-list/CardList.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Card } from '../card/Card';
+
+import './style.css';
+
+export const CardList = (props) => {
+  return (
+    <div className="card-list">
+      {' '}
+      {props.monsters.map((monster) => (
+        <Card key={monster.id} monster={monster} />
+      ))}
+    </div>
+  );
+};
